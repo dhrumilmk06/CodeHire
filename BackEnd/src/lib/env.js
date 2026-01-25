cdcdfcba2c48bde0b382b7164caf771f34d762bb
dhrumilmk06 <kayasthadm@gmail.com>
2026-01-26 02:39:03 +0530

    updating inngest

--- BackEnd/src/lib/env.js
+++ BackEnd/src/lib/env.js
@@ -1,9 +1,9 @@
 import env from 'dotenv';
 
-env.config({quiet: true}); // Load .env file contents into process.env
+env.config({ quiet: true }); // Load .env file contents into process.env
 
 export const ENV = {
-    PORT: process.env.PORT, 
+    PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
     NODE_ENV: process.env.NODE_ENV,
     INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
--- BackEnd/src/lib/inngest.js
+++ BackEnd/src/lib/inngest.js
@@ -1,18 +1,18 @@
-import{ Inngest } from 'inngest'
-import { connectDB }  from './db.js'
+import { Inngest } from 'inngest'
+import { connectDB } from './db.js'
 import User from '../models/User.js'
 
-export const inngest = new Inngest({id: 'codehire-app'})
+export const inngest = new Inngest({ id: 'codehire-app' })
 
 //syncUser function is used to connect clerk to mongo db for to store  user account and listens to user.created event from Clerk and creates a new user in our database
 const syncUser = inngest.createFunction(
-    {id: "sync-user"}, // we can give any id here
-    {event: 'user.created'}, // this is event in clerk that we are listening means whnever a new user is created in clerk this function will be triggered
-   
-    async ({event}) => { // this is the function that will be executed when the event is triggered
+    { id: "sync-user" }, // we can give any id here
+    { event: 'clerk/user.created' }, // this is event in clerk that we are listening means whnever a new user is created in clerk this function will be triggered
+
+    async ({ event }) => { // this is the function that will be executed when the event is triggered
         await connectDB()
         //this event id,email_addresses, first_name, last_name, image_url are give by cleark when user is created
-        const {id,email_addresses, first_name, last_name, image_url} = event.data
+        const { id, email_addresses, first_name, last_name, image_url } = event.data
 
         const newUser = {
             clerkId: id,
@@ -26,15 +26,15 @@ const syncUser = inngest.createFunction(
 )
 
 const deleteUserFromDB = inngest.createFunction(
-    {id: "delete-user"}, // we can give any id here
-    {event: 'user.deleted'}, 
+    { id: "delete-user" }, // we can give any id here
+    { event: 'clerk/user.deleted' },
 
-    async ({event}) => { // this is the function that will be executed when the event is triggered
+    async ({ event }) => { // this is the function that will be executed when the event is triggered
         await connectDB()
         //this event id is given by cleark when user is deleted
-        const {id} = event.data
+        const { id } = event.data
 
-        await User.deleteOne({clerkId: id});
+        await User.deleteOne({ clerkId: id });
     }
 )
 

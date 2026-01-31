e33c42e8268131e5fea0f760b475a6822cdae38b
dhrumilmk06 <kayasthadm@gmail.com>
2026-01-31 22:54:57 +0530

    frontend tools setup

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import {BrowserRouter} from 'react-router'
+import { QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 
 // Import your Clerk Publishable Key
@@ -14,8 +18,12 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-      </ClerkProvider>
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ FrontEnd/src/pages/ProblemPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ProblemPage = () => {
+  return (
+    <div>ProblemPage</div>
+  )
+}

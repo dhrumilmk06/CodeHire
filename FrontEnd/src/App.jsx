27beadb20a976dd7f108fdf4b61e9f8774dceeb3
dhrumilmk06 <kayasthadm@gmail.com>
2026-02-04 23:57:05 +0530

    adding session page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,11 @@
-import {Routes, Route, Navigate} from 'react-router'
-import { HomePage } from './pages/HomePage.jsx'
-import { ProblemsPage } from './pages/ProblemsPage'
-import { SignIn, useUser } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
 import { Toaster } from 'react-hot-toast';
+import { Navigate, Route, Routes } from 'react-router';
 import { DashBoardPage } from './pages/DashBoardPage.jsx';
+import { HomePage } from './pages/HomePage.jsx';
 import { ProblemPage } from './pages/ProblemPage.jsx';
+import { ProblemsPage } from './pages/ProblemsPage';
+import { SessionPage } from './pages/SessionPage.jsx';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
         <Route path='/dashboard' element={isSignedIn ? <DashBoardPage /> : <Navigate to= {"/"} /> } />
         <Route path='/problems' element={isSignedIn ? <ProblemsPage/> : <Navigate to = {'/'} />} />
         <Route path='/problem/:id' element={isSignedIn ? <ProblemPage/> : <Navigate to = {'/'} />} />
+        <Route path='/session/:id' element={isSignedIn ? <SessionPage/> : <Navigate to = {'/'} />} />
 
       </Routes>
       <Toaster />
